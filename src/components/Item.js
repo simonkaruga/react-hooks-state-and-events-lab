ba8ea79fed1ac0bc76d7caf5680bbbe9e9ad08c2
simonkaruga <simonkaruganjoroge@gmail.com>
2025-10-27 15:28:04 +0300

    Add otigin

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false);
+
   return (
-    <li className="">
+    <li className={inCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={() => setInCart((c) => !c)}>
+        Add to Cart
+      </button>
     </li>
   );
 }
